d468f206ec3780f372bc0babe95779eeba759e43
root <root@raspberrypi.(none)>
2014-05-28 13:29:08 +0000

    Start and stop capturing has been done~

--- routes/device.js
+++ routes/device.js
@@ -5,27 +5,54 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.send('respond with a device manager');
 });
-
 /* GET users listing. */
+router.get('/isOnWorking', function(req, res) {
+    var exec = require('child_process').exec,
+        child;
+
+    child = exec("pstree",
+        function (error, stdout, stderr) {
+            if (error !== null) {
+                res.json({success:false, error:stderr, error2:error});
+            }else{
+                if(stdout.indexOf("fswebcam")>=0){
+                    res.json({success:true, isworking:true});
+                }else{
+                    res.json({success:true, isworking:false});
+                }
+            }
+        }
+    );
+
+});
+
+
+/* Start timelapse capture image. */
 router.get('/start', function(req, res) {
     var exec = require('child_process').exec,
         child;
-   // var execCommand = './bash/fswebcam -r 1920x1080 -S 15 --jpeg 99 -p MJPEG --shadow --title "Images" --subtitle "By Charmyin" --info "Author: Charmyin" -q --save /mnt/usb/test';
+   /// / var execCommand = './bash/fswebcam -r 1920x1080 -S 15 --jpeg 99 -p MJPEG --shadow --title "Images" --subtitle "By Charmyin" --info "Author: Charmyin" -q --save /mnt/usb/test';
+
     var execCommand="./start.sh";
+    var commandTimeout;
     child = exec(execCommand,
         function (error, stdout, stderr) {
             if (error !== null) {
+                clearTimeout(commandTimeout);
                 res.json({success:false, error:stderr});
             }else{
+                clearTimeout(commandTimeout);
                 res.json({success:true, info:stdout});
             }
-        });
-    setTimeout(function() {
+        }
+    );
+    commandTimeout=setTimeout(function() {
         res.json({success:true});
-    }, 2000);
+    }, 3000);
+
 });
 
-/* GET users listing. */
+/* Stop capturing images. */
 router.get('/stop', function(req, res) {
     var exec = require('child_process').exec,
         child;
