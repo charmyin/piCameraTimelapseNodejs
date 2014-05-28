9ab7704afbbe4b4bef88a70d0c2967f7c12a2ad5
root <root@raspberrypi.(none)>
2014-05-28 22:39:30 +0000

    auto mount sda1 or sdb1 to /mnt/usb

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo mount /dev/sda1 /mnt/usb
+sudo mount /dev/sdb1 /mnt/usb
 contentFswebcam=`pstree | grep fswebcam`
 
 if [ -n "${contentFswebcam}" ]; then
@@ -8,6 +10,6 @@ else
   let currentFileName=lastFileName+1
   mkdir /mnt/usb/${currentFileName}
   ./fswebcam -r 1920x1080 -S 15 --jpeg 99 -p MJPEG --shadow --title "Images" --subtitle "By Charmyin" --info "Author: Charmyin" -q --save /mnt/usb/${currentFileName}/
-  #echo "./fswebcam -r 1920x1080 -S 15 --jpeg 99 -p MJPEG --shadow --title "Images" --subtitle "By Charmyin" --info "Author: Charmyin" -q --save /mnt/usb/${currentFileName}/"
+ # echo "./fswebcam -r 1920x1080 -S 15 --jpeg 99 -p MJPEG --shadow --title "Images" --subtitle "By Charmyin" --info "Author: Charmyin" -q --save /mnt/usb/${currentFileName}/"
 fi
 
